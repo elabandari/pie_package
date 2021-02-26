312315f7521d0d7b4ebc72108c1810c02c1e01c6
Ela Bandari <elabandari@gmail.com>
2021-02-26 10:29:43 -0800

    add tests

--- tests/test_pie_package.py
+++ tests/test_pie_package.py
@@ -2,4 +2,7 @@ from pie_package import __version__
 from pie_package import pie_package
 
 def test_version():
-    assert __version__ == 0.1.0
+    assert __version__ == '0.1.0'
+
+def test_show_pies():
+    assert type(pie_package.show_pie()) == type("str")
